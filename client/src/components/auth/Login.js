d74c8651dfc0b421ea0390c65981b1ef759a4c5d
krishnadev v <krishnadevv12@gmail.comgit config --global user.name krishnadev7>
2022-01-17 12:26:33 +0530

    Request example and login form successfully completed

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React,{Fragment,useState} from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [FormData, setFormData] = useState({
+      email: '',
+      password: '',
+    });
+
+    const {  email, password} = FormData;
+
+    const onChange = e =>
+      setFormData({ ...FormData, [e.target.name]: e.target.value });
+
+    const onSubmit = async e => {
+      e.preventDefault();
+      
+        console.log('Success');
+      
+    };
     return (
-        <div>
-            login
-        </div>
-    )
+      <Fragment>
+        <h1 className='large text-primary'>Sign In</h1>
+        <p className='lead'>
+          <i className='fas fa-user'></i> Sign Into Your Account
+        </p>
+        <form className='form' onSubmit={e => onSubmit(e)}>
+         
+          <div className='form-group'>
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              value={email}
+              onChange={e => onChange(e)}
+              required
+            />
+            
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Password'
+              name='password'
+              value={password}
+              onChange={e => onChange(e)}
+              required
+              minLength='6'
+            />
+          </div>
+         
+          <input type='submit' className='btn btn-primary' value='Login' />
+        </form>
+        <p className='my-1'>
+          Already have an account? <Link to='/register'>Sign up</Link>
+        </p>
+      </Fragment>
+    );
 }
 
 export default Login
