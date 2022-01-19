f2cda54ed71fac9720f0fee08e6aedb3c428b97d
krishnadev v <krishnadevv12@gmail.comgit config --global user.name krishnadev7>
2022-01-19 23:05:27 +0530

    logout & navbar links completed with bugs

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -95,4 +96,10 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// LOGOUT
+
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -6,3 +6,4 @@ export const USER_LOADED = 'USER_LOADED';
 export const LOGIN_SUCCESS = 'AUTH_ERROR';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGOUT = 'LOGOUT';
