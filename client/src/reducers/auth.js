5d7d6c5fb4512a2d5e6be9d72534afa496c394a7
krishnadev v <krishnadevv12@gmail.comgit config --global user.name krishnadev7>
2022-01-19 12:00:11 +0530

    load user & set auth token completed withr

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+import { REGISTER_SUCCESS, REGISTER_FAIL,USER_LOADED,AUTH_ERROR } from '../actions/types';
 
 const initialState = {
     token: localStorage.getItem('token'),
@@ -10,6 +10,13 @@ export default function(state = initialState,action) {
     const {type,payload} = action;
 
     switch (type) {
+        case USER_LOADED:
+            return{
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            }
         case REGISTER_SUCCESS:
                 localStorage.setItem('token', payload.token)
             return {
@@ -18,8 +25,9 @@ export default function(state = initialState,action) {
                 isAuthenticated: true,
                 loading: false
             }
-    
+        
         case REGISTER_FAIL:
+        case AUTH_ERROR:  
                 localStorage.removeItem('token');
             return {
                 ...state,
