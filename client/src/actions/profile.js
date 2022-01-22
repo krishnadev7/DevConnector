b38a95fd921f5f86a8aee0c76f23066428c868fc
krishnadev v <krishnadevv12@gmail.comgit config --global user.name krishnadev7>
2022-01-22 16:09:12 +0530

    Create Profile Action commited with no bugs

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,23 +1,61 @@
-import axios from 'axios'
-import { SET_ALERT } from './types'
+import axios from 'axios';
+import { setAlert } from './alert';
+import { SET_ALERT } from './types';
 
-import { GET_PROFILE,PROFILE_ERROR } from './types'
+import { GET_PROFILE, PROFILE_ERROR } from './types';
 
 // GET current users profile
 
 export const getCurrentProfile = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/profile/me');
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const createProfile =
+  (formData, history, edit = false) =>
+  async dispatch => {
     try {
-        const res = await axios.get('/api/profile/me')
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
 
-        dispatch({
-            type: GET_PROFILE,
-            payload: res.data
-        });
+      const res = await axios.post('/api/profile', formData, config);
 
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      });
+
+      dispatch(
+        setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success')
+      );
+
+      if (!edit) {
+        history.push('/dashboard');
+      }
     } catch (err) {
-        dispatch({
-            type: PROFILE_ERROR,
-            payload: {msg: err.response.statusText, status: err.response.status }
-        })
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+      }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      });
     }
-}
+  };
