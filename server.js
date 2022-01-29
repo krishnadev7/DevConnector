74e812b9d8687f8fb7b5978c95a3a2f1feeb7735
krishnadev v <krishnadevv12@gmail.comgit config --global user.name krishnadev7>
2022-01-29 20:10:57 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 // Connecting DataBase
 connectDB();
 // init middleware
-app.use(express.json({ extended:false }))
-
-app.get('/', (req, res) => res.send('API running'));
+app.use(express.json({ extended: false }));
 
 //Define Routes
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/posts', require('./routes/api/posts'))
-app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
